80f98a62bac3a07b7df9f921f470c88efd51e000
kigure <102130835+kigure@users.noreply.github.com>
2023-02-27 06:18:32 +0900

    Add lucky juice

---
+++ omikuji.py
@@ -0,0 +1,8 @@
+import random
+
+lucky = random.randint(1, 5)
+
+if lucky == 1:
+    print("You are lucky")
+else:
+    print("You are not lucky")
--- vending_machine.py
+++ vending_machine.py
@@ -3,13 +3,24 @@
 # trueの場合はジュースが出て、falseはジュースは出ずに足りない金額を告知
 # ジュースの値段より多く金額が投入された場合は、ジュースが出てかつ money - JUICE の値が出力
 
+import random
+
+lucky = random.randint(1, 3)
+
 JUICE = 150
 money = input("いくら入れますか?:")
 money = int(money)
 
-if money > JUICE:
+if money > JUICE and lucky == 1:
+    print("ジュースゲット")
+    print("当たりです")
+    print(str(money - JUICE) + "円のお釣りです")
+elif money > JUICE:
     print("ジュースゲット")
     print(str(money - JUICE) + "円のお釣りです")
+elif money == JUICE and lucky == 1:
+    print("ジュースゲットお釣りなし")
+    print("当たりです")
 elif money == JUICE:
     print("ジュースゲットお釣りなし")
 else:
